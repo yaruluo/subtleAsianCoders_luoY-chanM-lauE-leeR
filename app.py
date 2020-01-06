1c0964e5f3f9763e1a416782015ab275590f56ca
Yaru Luo <yluo00@stuy.edu>
2020-01-06 03:18:46 -0500

    reorg

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
-'''
-subtleAsianCoders - Yaru Luo, Eric "Morty" Lau, Raymond Lee, Matthew Chan
-SoftDev1 pd1
-P02 -- The End
-'''
-
-from flask import Flask, request, redirect, session, render_template, url_for, flash
-import sqlite3
-import urllib.request
-import urllib.parse
-import functools
-import os
-import json
-import datetime
-
-app = Flask(__name__)
-app.secret_key = os.urandom(32)
-
-@app.route('/')
-def root():
-    return render_template(
-        'index.html'
-    )
+from app import app
+from app.utl import build_db
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    build_db()
+    app.run(debug=True)
