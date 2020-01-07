cd3f7e169275463b662046835a0bc59124703da9
ray. lee <rlee00@stuy.edu>
2020-01-07 02:00:22 -0500

    made table names singular

--- app/utl/models.py
+++ app/utl/models.py
@@ -2,30 +2,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Songs(db.Model):
+class Song(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
     artist = db.Column(db.Text, nullable=False)
     title = db.Column(db.Text, nullable=False)
     genre = db.Column(db.Text, nullable=False)
     inalbum = db.Column(db.Integer, nullable=False)
 
-class Albums(db.Model):
+class Album(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
     aid = db.Column(db.Integer, nullable=False)
     title = db.Column(db.Text, nullable=False)
 
-class Lyrics(db.Model):
+class Lyric(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
     lyric = db.Column(db.Text, nullable=False)
 
-class Listens(db.Model):
+class Listen(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
     numlisten = db.Column(db.Integer, nullable=False)
 
-class AlbumCoverArts(db.Model):
+class AlbumCoverArt(db.Model):
     aid = db.Column(db.Integer, primary_key=True)
     coverartlink = db.Column(db.Text, nullable=False)
 
-class SingleCoverArts(db.Model):
+class SingleCoverArt(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
     coverartlink = db.Column(db.Text, nullable=False)
