a15d99d6a8cb2cb08c26865a4a7804649361d722
ray. lee <rlee00@stuy.edu>
2020-01-07 08:23:33 -0500

    updated models

--- app/utl/models.py
+++ app/utl/models.py
@@ -4,15 +4,14 @@ db = SQLAlchemy()
 
 class Song(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
-    aid = db.Column(db.Integer, nullable=False)
+    aid = db.Column(db.Integer, primary_key=True)
     artist = db.Column(db.Text, nullable=False)
     title = db.Column(db.Text, nullable=False)
     genre = db.Column(db.Text, nullable=False)
     lyrics = db.Column(db.Text, nullable=False)
-    numlisten = db.Column(db.Integer, nullable=False)
+    numListen = db.Column(db.Integer, nullable=False)
 
 class Album(db.Model):
     aid = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, nullable=False)
     coverartlink = db.Column(db.Text, nullable=False)
-
