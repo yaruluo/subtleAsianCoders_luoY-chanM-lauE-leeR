87fe52f702d7824ccc87971ea381941751f6012d
ray. lee <rlee00@stuy.edu>
2020-01-07 08:15:42 -0500

    updated models

--- app/utl/models.py
+++ app/utl/models.py
@@ -4,28 +4,15 @@ db = SQLAlchemy()
 
 class Song(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
+    aid = db.Column(db.Integer, nullable=False)
     artist = db.Column(db.Text, nullable=False)
     title = db.Column(db.Text, nullable=False)
     genre = db.Column(db.Text, nullable=False)
-    inalbum = db.Column(db.Integer, nullable=False)
-
-class Album(db.Model):
-    sid = db.Column(db.Integer, primary_key=True)
-    aid = db.Column(db.Integer, nullable=False)
-    title = db.Column(db.Text, nullable=False)
-
-class Lyric(db.Model):
-    sid = db.Column(db.Integer, primary_key=True)
-    lyric = db.Column(db.Text, nullable=False)
-
-class Listen(db.Model):
-    sid = db.Column(db.Integer, primary_key=True)
+    lyrics = db.Column(db.Text, nullable=False)
     numlisten = db.Column(db.Integer, nullable=False)
 
-class AlbumCoverArt(db.Model):
+class Album(db.Model):
     aid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
     coverartlink = db.Column(db.Text, nullable=False)
 
-class SingleCoverArt(db.Model):
-    sid = db.Column(db.Integer, primary_key=True)
-    coverartlink = db.Column(db.Text, nullable=False)
