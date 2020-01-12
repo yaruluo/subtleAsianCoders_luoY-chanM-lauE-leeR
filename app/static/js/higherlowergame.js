6a02c6ac0c6309cb64a2b27453f3ac77fc1084c1
ray. lee <rlee00@stuy.edu>
2020-01-12 01:32:37 -0500

    added comments

--- app/static/js/higherlowergame.js
+++ app/static/js/higherlowergame.js
@@ -1,49 +1,39 @@
-function init(songs, counter) {
-  var hasLost = false;
+function init(songs, counter) { // params are meant to preserve the state of the game throughout playing; songs is the list of songs (with song title, artist, popularity rating, cover art link, etc.) and counter is the current index in the songs list that the game is on. Initially, the game is on the 0th song, so on first load, counter should equal 0.
   $(document).ready(() => {
-    var nextPageIndex = parseInt($("#higher-btn").attr("next-page"));
-    // console.log(counter);
+    // when the document is ready to be loaded, run code below
     loadGame(songs, counter, 0);
     $("#higher-btn, #lower-btn").on("click", btn => {
-      // console.log(counter);
-      // console.log($("left-song-popularity").text())
-      // console.log($("right-song-popularity").text())
-      var leftSongPopularity = parseInt($("#left-song-popularity").text());
-      var rightSongPopularity = parseInt($("#right-song-popularity").text());
-      // console.log(parseInt(leftSongPopularity));
-      // console.log(parseInt(rightSongPopularity));
-      if (counter >= 8) $(".container").hide(); // finished all 10 questions
+      var leftSongPopularity = parseInt($("#left-song-popularity").text()); // get the left song popularity rating
+      var rightSongPopularity = parseInt($("#right-song-popularity").text()); //get the right song popularity rating
+      if (counter >= 8) $(".container").hide(); // this means the user finished all 10 questions; hide the page so they cannot play anymore.
       // scorekeeping
       if (
-        (leftSongPopularity <= rightSongPopularity && // right choice
+        (leftSongPopularity <= rightSongPopularity && // if the user chooses the 'Higher' choice and the right song is indeed higher in popularity than the left song, or if the user chooses the 'Lower' choice and the right song is indeed lower in popularity than the left song, then consider that the user picked the right choice and move on to the next question (if applicable)
           $(btn.target).html() == "Higher") ||
         (leftSongPopularity >= rightSongPopularity &&
           $(btn.target).html() == "Lower")
       ) {
-        curScore = parseInt($("#score").text());
+        curScore = parseInt($("#score").text()); // gets the current score of the user
         // $(".left-half, .right-half").animate({right: '50%'});
-        $(btn.target).attr("next-page", ++counter);
-        loadGame(songs, counter, ++curScore);
-      } else { // wrong choice
-        $("#higher-lower").empty()
+        $(btn.target).attr("next-page", ++counter); // set the counter equal to counter + 1 and then return its updated value here.
+        loadGame(songs, counter, ++curScore); // song and counter is explained above; curScore (current score) is incremented and then its value is returned and loaded back into loadGame for the next question (if applicable—user did not finish all 10 questions yet)
+      } else {
+        // wrong choice
+        $("#higher-lower").empty();
         // Implement end game screen
       }
     });
   });
 }
 
-function loadGame(songs, counter, score) {
-  $("#left-image").attr(
-    "src", `${songs[counter]["coverArtLink"]}`
-  )
+function loadGame(songs, counter, score) { // this fxn basically updates the moving parts of the game: the left and right images, song titles, song artists, popularity ratings, etc.; this modularization allows us to call this function again and again after a button is clicked—to update the DOM each time upon an event.
+  $("#left-image").attr("src", `${songs[counter]["coverArtLink"]}`);
 
   $("#left-song-title").html(`${songs[counter]["title"]}`);
   $("#left-song-artist").html(`${songs[counter]["artist"]}`);
   $("#left-song-popularity").html(`${songs[counter]["popularity"]}`);
 
-  $("#right-image").attr(
-    "src", `${songs[counter + 1]["coverArtLink"]}`
-  )
+  $("#right-image").attr("src", `${songs[counter + 1]["coverArtLink"]}`);
 
   $("#right-song-title").html(`${songs[counter + 1]["title"]}`);
   $("#right-song-artist").html(`${songs[counter + 1]["artist"]}`);
